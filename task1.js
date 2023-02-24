c9b92c913d2b52d20eb5f50975bb5edb4ebca879
vaibhav <itsmehvaibhav@gmail.com>
2023-02-24 09:37:20 +0530

    getelementbyid

---
+++ task1.js
@@ -0,0 +1,9 @@
+//GETELEMENTBYID
+// Select the header title element and add a black border
+const headerTitle = document.getElementById("header-title");
+headerTitle.style.border = "1px solid black";
+
+// Select the Add Items element and make it bold with green font color
+const addItems = document.querySelector(".title");
+addItems.style.color = "green";
+addItems.style.fontWeight = "bold";
