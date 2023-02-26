49c7d7a78f434e18f83813c82cde5118df8fc2b0
itsmehvaibhav <96306670+itsmehvaibhav@users.noreply.github.com>
2023-02-26 13:42:09 +0530

    Update localstorage(moreusers).js

--- localstorage(moreusers).js
+++ localstorage(moreusers).js
@@ -2,38 +2,45 @@ const myForm = document.querySelector('#myForm');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const messageInput = document.querySelector('#message');
-
-// Check if there is existing data in local storage
-let formDataArray = JSON.parse(localStorage.getItem('formDataArray')) || [];
+const formDataList = document.querySelector('#formDataList');
 
 myForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  
-  // Create an object with the form data
+
+  // Retrieve the existing form data from local storage, or initialize an empty array if it doesn't exist
+  let formDataArray = JSON.parse(localStorage.getItem('formDataArray')) || [];
+
+  // Create an object with the new form data
   const formData = {
     name: nameInput.value,
     email: emailInput.value,
     message: messageInput.value
   };
-  
-  // Add the form data object to the array
+
+  // Add the new form data to the array of existing form data
   formDataArray.push(formData);
-  
-  // Convert the array to a JSON string and store it in local storage
+
+  // Store the updated array of form data in local storage
   localStorage.setItem('formDataArray', JSON.stringify(formDataArray));
-  
+
   // Display a message to the user
   alert('Form data saved!');
-  
-  // Reset the form inputs
-  nameInput.value = '';
-  emailInput.value = '';
-  messageInput.value = '';
+
+  // Update the list of form data on the page
+  updateFormDataList(formDataArray);
 });
 
-// Display the stored form data in a list
-formDataArray.forEach((formData) => {
-  const listItem = document.createElement('li');
-  listItem.innerHTML = `${formData.name} (${formData.email}): ${formData.message}`;
-  document.querySelector('#formDataList').appendChild(listItem);
-});
+function updateFormDataList(formDataArray) {
+  // Clear the existing list items
+  formDataList.innerHTML = '';
+
+  // Loop through the array of form data and create a new list item for each object
+  formDataArray.forEach(formData => {
+    const li = document.createElement('li');
+    li.textContent = `Name: ${formData.name}, Email: ${formData.email}, Message: ${formData.message}`;
+    formDataList.appendChild(li);
+  });
+}
+
+// Call the updateFormDataList function when the page loads to display any existing form data
+updateFormDataList(JSON.parse(localStorage.getItem('formDataArray')) || []);
